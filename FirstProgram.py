d0c79439cf3f1ec62a7705e375b4b5d76c56b03a
klu2300031388 <2300031388@kluniversity.in>
2025-07-21 10:47:50 +0530

    first commit

---
+++ FirstProgram.py
@@ -0,0 +1,17 @@
+
+print("Hiii bestie 💕 I'm learning Python!!")
+print("Hello, World")
+print("Hello Everybody," "My name is Ramya")
+
+print(10)
+print(15/5)
+print(10%5)
+
+print(int("5"))
+
+
+# address knowing purpose
+x=5
+print(id(x))
+
+
